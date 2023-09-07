a30294acc22abfe17294beacbd2a7c9088deea02
james <james@bubbleye.com>
2023-09-07 14:59:18 +0800

    Catch error and handle that some storms dont have invests

--- generate_storm_plots.py
+++ generate_storm_plots.py
@@ -8,33 +8,41 @@ from plot import plot_storm
 
 
 def main() -> None:
-    realtime_obj = realtime.Realtime(jtwc=True, jtwc_source="ucar")
+    jtwc_data_sources = ["ucar"]
 
-    active_storms = realtime_obj.list_active_storms()
+    for data_source in jtwc_data_sources:
+        realtime_obj = realtime.Realtime(jtwc=True, jtwc_source=data_source)
 
-    date_str = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+        active_storms = realtime_obj.list_active_storms()
 
-    print(f"Found {active_storms=}")
+        date_str = datetime.datetime.utcnow().strftime("%Y-%m-%d")
 
-    my_dir = f"{IMAGES_DIR}/{date_str}"
+        print(f"Found {active_storms=}")
 
-    for storm_id in active_storms:
-        print(f"start {storm_id}")
-        storm = realtime_obj.get_storm(storm_id)
-        storm_forecast = storm.get_forecast_realtime(ssl_certificate=False)
-        storm_forecast["already_forcasted"] = [
-            (datetime.timedelta(hours=x) + storm_forecast["init"]) <= storm.time.max()
-            for x in storm_forecast["fhr"]
-        ]
-        pathlib.Path(f"{my_dir}/{storm_id}").mkdir(parents=True, exist_ok=True)
-        try:
-            storm.plot_forecast_realtime(
-                save_path=f"{my_dir}/{storm_id}/tropycal_forecast_realtime.jpg"
-            )
-            fig = plot_storm(storm, storm_forecast)
-            fig.savefig(f"{my_dir}/{storm_id}/my_plot.jpg")
-        except Exception as e:
-            print(f"JPG Caught exception {e}")
+        my_dir = f"{IMAGES_DIR}/{date_str}"
+
+        for storm_id in active_storms:
+            print(f"start {storm_id}")
+            storm = realtime_obj.get_storm(storm_id)
+            try:
+                storm_forecast = storm.get_forecast_realtime(ssl_certificate=False)
+            except Exception as e:
+                print(f"Get forecast caught exception {e}")
+                continue
+            storm_forecast["already_forcasted"] = [
+                (datetime.timedelta(hours=x) + storm_forecast["init"])
+                <= storm.time.max()
+                for x in storm_forecast["fhr"]
+            ]
+            pathlib.Path(f"{my_dir}/{storm_id}").mkdir(parents=True, exist_ok=True)
+            try:
+                storm.plot_forecast_realtime(
+                    save_path=f"{my_dir}/{storm_id}/tropycal_forecast_realtime.jpg"
+                )
+                fig = plot_storm(storm, storm_forecast)
+                fig.savefig(f"{my_dir}/{storm_id}/{data_source}_myplot.jpg")
+            except Exception as e:
+                print(f"JPG Caught exception {e}")
 
 
 if __name__ == "__main__":
